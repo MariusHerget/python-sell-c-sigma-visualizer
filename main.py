aca8046f9222636d9def40788ba1099f5512f793
Marius Herget <me@mariusherget.de>
2019-09-10 17:45:28 +0200

    main to fit new modules

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import random as rand
 import math
 
+
 import modules.sparse_matrix_generator as smgen
 import modules.matrix_visulizer as mvis
 # SELL-4-σspMVM  kernelwith four-way unrolling
@@ -19,17 +20,12 @@ import modules.matrix_visulizer as mvis
 # }
 
 def main():
-    print("Which size should the matrix have?")
-    # print("x")
-    # x = int(input())
-    # print("y")
-    # y = int(input())
     x=6
     y=6
-    # for ix in range(x):
-    #     for iy in range(y):
-    #         print()
-    mvis.printmatrix(smgen.create(16,16))
+    mvis.matprint(smgen.create(16,16))
+    print()
+    mvis.matprint(smgen.diag_prob(16,16))
+    # smgen.diag_prob(10,10)
 
 
 if __name__ == "__main__":
