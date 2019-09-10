26957c7ba08cfae0984e92ec6e3c38b3612dd8c4
Marius Herget <me@mariusherget.de>
2019-09-10 17:44:52 +0200

    new beautiful, colored printer for normal matrixes

--- modules/matrix_visulizer.py
+++ modules/matrix_visulizer.py
@@ -1,10 +1,12 @@
 import numpy as np
 import random as rand
 import math
+from termcolor import colored
 
 def printmatrix(m):
     x,y = m.shape
     if x != 0 and y !=0:
+        print_coordinates_x(x)
         printline(y)
         for ix in range(x):
             t = "#  "
@@ -26,3 +28,26 @@ def printline(y):
         str+="#####"
     str+="##"
     print(str)
+
+def print_coordinates_x(x):
+    t = "   "
+    for i in range(x):
+        t+=" "+str(i)
+        if (x<10):
+            t+=" "
+        t+="  "
+    print(t)
+
+def matprint(mat, fmt="g"):
+    col_maxes = [max([len(("{:"+fmt+"}").format(x)) for x in col]) for col in mat.T]
+    row,col = np.diag_indices(mat.shape[0])
+    for idx, x in enumerate(mat):
+        for i, y in enumerate(x):
+            c = "white"
+            if y == 0:
+                c = "magenta"
+            # print(idx,i, row[idx], col[i])
+            if idx == row[idx] and i == col[idx]:
+                c= "red"
+            print(colored(("{:"+str(col_maxes[i])+fmt+"}").format(y), c), end="  ")
+        print(" ")
