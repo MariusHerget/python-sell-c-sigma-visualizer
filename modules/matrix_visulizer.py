bugfix wrong index mapping bc matrix generation was wrong

modules/matrix_visulizer.py
@@ -69,25 +69,35 @@ def matprint(mat, highlight_x=-1, highlight_y=-1, sellcsigma=None, isigma=None, 
 
 def print_sellcsigma_matrix(sell_c_sigma, highlight_x=-1, highlight_y=-1):
     print("\nPrinting SELL-" + str(sell_c_sigma.sell_c) +
-          "-" + str(sell_c_sigma.sell_sigma))
+          "-" + str(sell_c_sigma.sell_sigma), end=" ")
 
-    print(colored("U  | S  | C  | R", "cyan"), end="\n")
-    scope_index, chunk_offset, row_index, sell_x = sell_c_sigma.global_index_to_sell_index(
-        highlight_x, highlight_y)
+    if highlight_x > -1 and highlight_y > -1:
+        scope_index, chunk_offset, row_index, sell_x = sell_c_sigma.global_index_to_sell_index(
+            highlight_x, highlight_y)
+        print("with highlighted point: (x= " +
+              str(highlight_x) + ", y= " + str(highlight_y) + ")", end=" ")
+        print("Scope: "+str(scope_index)+" Chunk: "+str(chunk_offset)+" Row: "+str(row_index)+" x:"+str(sell_x), end="\n")
+    else:
+        print("\n", end="")
     # print("\nprint_sellcsigma_matrix")
     # print("scope_index: "+str(scope_index))
     # print("chunk_offset: "+str(chunk_offset))
     # print("row_index: "+str(row_index))
     # print("sell_x: "+str(sell_x))
     # print(sell_c_sigma.global_index_to_sell_value(highlight_x, highlight_y))
+    print(colored("U  | S  | C  | R", "cyan"), end="\n")
     for isigma, sigma in enumerate(sell_c_sigma.m_final):
         for ichunk, chunk in enumerate(sigma):
-            if isigma == scope_index and ichunk == chunk_offset:
-                kx = sell_x
-                ky = row_index
+            if highlight_x > -1 and highlight_y > -1:
+                if isigma == scope_index and ichunk == chunk_offset:
+                    kx = sell_x
+                    ky = row_index
+                else:
+                    kx = -1
+                    ky = -1
+                matprint(np.array(chunk), kx, ky, sell_c_sigma, isigma, ichunk)
             else:
-                kx = -1
-                ky = -1
-            matprint(np.array(chunk), kx, ky, sell_c_sigma, isigma, ichunk)
+                matprint(np.array(chunk), -1, -1, sell_c_sigma, isigma, ichunk)
+
             # for irow, row in enumerate(chunk):
             #     print(row)

modules/sparse_matrix_generator.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 def create(x, y):
     nonzero = x * y - calc_sparsity(x * y)
-    m_origin = np.zeros((x, y))
-    print("Generate matrix " + str((x, y)) +
-          " with " + str(nonzero) + " values.")
+    m_origin = np.zeros((y, x))
+    print("Generate matrix (" + str(y) + " rows with each " + str(x) + " elements"
+          ") with " + str(nonzero) + " values.")
     return fill_matrix_with_random(m_origin, nonzero)
 
 
@@ -19,22 +19,22 @@ def calc_sparsity(n):
 
 
 def diag_prob(or_x, or_y):
-    m_origin = np.zeros((or_x, or_y))
+    m_origin = np.zeros((or_y, or_x))
     nonzero = math.ceil(or_x * or_y - calc_sparsity(or_x * or_y))
-    print("Generate multivariate normal matrix " +
-          str((or_x, or_y)) + " with " + str(nonzero) + " values.")
-    mean = [math.ceil(or_x / 2) - 1, math.ceil(or_y / 2)]
+    print("Generate multivariate normal matrix (" + str(or_y) + " rows with each " + str(or_x) + " elements"
+          + ") with " + str(nonzero) + " values.")
+    mean = [math.ceil(or_y / 2) - 1, math.ceil(or_x / 2)]
     # diagonal covariance, points lie on x or y-axis
-    cov = [[or_x / 2, 0], [0, or_y]]
+    cov = [[or_y / 2, 0], [0, or_x]]
     x, y = np.random.multivariate_normal(mean, cov, nonzero).T
-    for ix in range(len(x)):
+    for ix in range(len(y)):
         # rotate point 45° to left to be in diagonal
         x[ix], y[ix] = rotate_point(
             x[ix], y[ix], -45, math.ceil(or_x / 2) - 1, math.ceil(or_y / 2))
         valx = math.ceil(x[ix])
         valy = math.ceil(y[ix])
-        if or_x > valx >= 0 and or_y > valy >= 0 and m_origin[valx][valy] == 0:
-            m_origin[valx][valy] = rand_num()
+        if or_x > valx >= 0 and or_y > valy >= 0 and m_origin[valy][valx] == 0:
+            m_origin[valy][valx] = rand_num()
             nonzero -= 1
     m_origin, usedNonZero = fill_matrix_diagonal_with_random(m_origin, 0.75)
     nonzero += usedNonZero